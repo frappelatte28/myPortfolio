cf18dd9d8fc72f601b86d06ae406f24202c86eb1
frappelatte28 <64382399+frappelatte28@users.noreply.github.com>
2020-06-03 03:35:52 +0530

    about me section and universal image loader
    use of flexbox(images doesnt work for a image )
    used word wrap property for text responsiveness
    learnt object fit property for image responsiveness
    learnt boxsixing for rendering including padding and margin
    js-learnt a new event generator that cause a particular fucntiion after  a given time
    set interval or window.set interval also leanrt how to stop that set interval using clearinterval or window.clear interval
    implemented universal image loader

--- js/loader.js
+++ js/loader.js
@@ -1,21 +1,48 @@
+ var imageLoaded = 0
  const loaderTime = 3000
+ var t = Date.now()
 
  const loader = document.querySelector('.loader')
  const main = document.querySelector('#main')
-const  testImage = document.querySelector('#testImage')
+ const  testImage = document.querySelector('#testImage')
+ const aboutMeImage = document.querySelector('.about_me_image > img')
 
- testImage.addEventListener('load', handleLoaded) ;
+ aboutMeImage.addEventListener('load',handleAboutMeOnLoad)
+ testImage.addEventListener('load', handleTestImageOnLoad) 
+ main.style.display ="none"
 
- main.style.display = "none"
+var setIntervalId = setInterval(handler, 1)
 
-
-
- function handleLoaded(){
-     window.setTimeout(handleTimeOut, loaderTime)
+  
+ function handler(){
+    if(imageLoaded == 2){
+        loader.style.display = "none"
+        main.style.display = "block"
+         clearInterval(setIntervalId)
+    }
+   
  }
 
-
- function handleTimeOut(){
-     loader.style.display = "none"
-     main.style.display = "block"
+ function handleAboutMeOnLoad(){
+    imageLoaded++ ;
 }
+
+function handleTestImageOnLoad(){
+    imageLoaded++;
+}
+
+
+
+//  main.style.display = "none"
+
+
+
+//  function handleLoaded(){
+//      window.setTimeout(handleTimeOut, loaderTime)
+//  }
+
+
+//  function handleTimeOut(){
+//      loader.style.display = "none"
+//      main.style.display = "block"
+// }
