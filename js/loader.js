5ff1d4f3cfd64cbd4acf358f02ffa451e8baf4b3
frappelatte28 <64382399+frappelatte28@users.noreply.github.com>
2020-06-02 23:09:03 +0530

    replaced loader and minor bugs fixed
    replaced loader
    learnt how to centerise element without using dispaly flex.

--- js/loader.js
+++ js/loader.js
@@ -1,21 +1,21 @@
-const loaderTime = 6000
+ const loaderTime = 3000
 
-const loader = document.querySelector('.LOADER')
-const main = document.querySelector('#main')
+ const loader = document.querySelector('.loader')
+ const main = document.querySelector('#main')
 const  testImage = document.querySelector('#testImage')
 
-testImage.addEventListener('load', handleLoaded) ;
+ testImage.addEventListener('load', handleLoaded) ;
 
-main.style.display = "none"
-
-
-
-function handleLoaded(){
-    window.setTimeout(handleTimeOut, loaderTime)
-}
+ main.style.display = "none"
 
 
-function handleTimeOut(){
-    loader.style.display = "none"
-    main.style.display = "block"
+
+ function handleLoaded(){
+     window.setTimeout(handleTimeOut, loaderTime)
+ }
+
+
+ function handleTimeOut(){
+     loader.style.display = "none"
+     main.style.display = "block"
 }
